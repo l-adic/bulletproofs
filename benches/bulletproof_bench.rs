dc93768695deac06d7c50c2376d5c21821b98729
martyall <martyall@protonmail.com>
2025-09-02 20:43:00 -0700

    replace bench

--- benches/bulletproof_bench.rs
+++ benches/bulletproof_bench.rs
@@ -133,5 +133,5 @@ fn bench_range_proofs(c: &mut Criterion) {
     bench_range_prove_verify_cycle::<64>(c, &shared_crs);
 }
 
-criterion_group!(benches,  bench_range_proofs);
+criterion_group!(benches, bench_ipa_prove_verify_cycle, bench_range_proofs);
 criterion_main!(benches);
